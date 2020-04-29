77a50e39f47e244deb93e5a887a122b0f77df666
Darren <darren.doyler@gmail.com>
2020-04-29 10:28:54 +0100

    Final Test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>testetstets</p>
+        <p>Final Test</p>
         <a
           className="App-link"
           href="https://reactjs.org"
