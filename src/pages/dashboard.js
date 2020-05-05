3298bd34e5d97e0cd066d66848ae42680ccf2e3e
Darren <darren.doyler@gmail.com>
2020-05-05 17:25:27 +0100

    Functional Routing

---
+++ src/pages/dashboard.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "../App.css";
+
+class Dashboard extends Component {
+  render() {
+    return <div>Hello World</div>;
+  }
+}
+
+export default Dashboard;
